1e4bcae7ab09862507caa197fe250fc0d22e5c3d
takuya <takuya.1st+nospam@gmail.com>
2022-04-04 04:54:49 +0900

    update

--- update-known-public-dns.sh
+++ update-known-public-dns.sh
@@ -14,6 +14,7 @@ function main() {
   echo start
 
   list=$(get_known_dns_list | sort | uniq)
+  echo "$list"
   echo "$list" | xargs -0 | grep 'tls://' | tee dns-list-dot.txt
   echo "$list" | xargs -0 | grep 'https://' | tee dns-list-doh.txt
   echo "$list" | xargs -0 | grep -P '^\d+\.\d+\.\d+\.\d+' | sort -t . -n | tee dns-list-ipv4.txt

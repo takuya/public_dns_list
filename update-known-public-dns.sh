d5003320977c5643cc88af08c247b7f70431204c
takuya <takuya.1st+nospam@gmail.com>
2022-04-04 04:50:20 +0900

    update

--- update-known-public-dns.sh
+++ update-known-public-dns.sh
@@ -8,12 +8,15 @@ function get_known_dns_list(){
 function main() {
 
   list=$(get_known_dns_list | sort | uniq)
-  echo "$list" | xargs -0 | grep 'tls://' > dns-list-dot.txt
-  echo "$list" | xargs -0 | grep 'https://' > dns-list-doh.txt
-  echo "$list" | xargs -0 | grep -P '^\d+\.\d+\.\d+\.\d+' | sort -t . -n > dns-list-ipv4.txt
-  echo "$list" | xargs -0 | grep -P '^\[?[a-fA-F0-9:\]]+$' | sort -t :  -n > dns-list-ipv6.txt
+
+  echo start
+  echo "$list" | xargs -0 | grep 'tls://' | tee dns-list-dot.txt
+  echo "$list" | xargs -0 | grep 'https://' | tee dns-list-doh.txt
+  echo "$list" | xargs -0 | grep -P '^\d+\.\d+\.\d+\.\d+' | sort -t . -n | tee dns-list-ipv4.txt
+  echo "$list" | xargs -0 | grep -P '^\[?[a-fA-F0-9:\]]+$' | sort -t :  -n | tee dns-list-ipv6.txt
 
 }
+
 
 
 main;

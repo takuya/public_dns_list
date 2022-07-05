cf46faf02df12e2b4775e1b4a372bda6b7592fcb
takuya <55338+takuya@users.noreply.github.com>
2022-07-06 01:31:33 +0900

    update-urls

--- update-known-public-dns.sh
+++ update-known-public-dns.sh
@@ -2,7 +2,8 @@
 
 
 function get_known_dns_list(){
-  curl -s 'https://kb.adguard.com/en/general/dns-providers#adguard-dns' | xmllint --html --xpath '//code/text()' - 2>/dev/null
+  curl -s -L 'https://kb.adguard.com/en/general/dns-providers#adguard-dns' \
+  | xmllint --html --xpath '//code/text()' - 2>/dev/null
 }
 function main() {
 

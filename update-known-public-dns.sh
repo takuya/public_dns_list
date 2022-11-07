46ea583333dc44ed4a5994dfcc34d4af9ca5bcfc
takuya <55338+takuya@users.noreply.github.com>
2022-11-07 15:58:03 +0900

    update dos as ip address for ipset

--- update-known-public-dns.sh
+++ update-known-public-dns.sh
@@ -5,6 +5,49 @@ function get_known_dns_list(){
   curl -s -L 'https://kb.adguard.com/en/general/dns-providers#adguard-dns' \
   | xmllint --html --xpath '//code/text()' - 2>/dev/null
 }
+function doh_to_ip(){
+  ignore=$(ignore_list)
+  list_names=($(cat dns-list-doh.txt  \
+    | sed 's|https://||'   \
+    | sed 's|/.*$||'       \
+    | sed -E 's|:[0-9]+||' \
+    | grep -vE "${ignore}"  \
+  ))
+  list_1=$( echo ${list_names[@]} \
+    | xargs -t -n 1 dig +short a \
+    | grep -v '[a-z]' 
+  )
+  list_2=$( echo $list_names | xargs -n1 echo |  grep  -E '[0-9]{1,3}$')
+  for i in $( echo $list_1 $list_2 | sort -n | uniq ); do echo $i; done  | tee dns-list-doh-as-ip.txt
+}
+function dot_to_ip(){
+  ignore=$(ignore_list)
+  list_names=($(cat dns-list-dot.txt  \
+    | sed 's|tls://||'   \
+    | sed 's|/.*$||'       \
+    | sed -E 's|:[0-9]+||' \
+    | grep -vE "${ignore}"  \
+  ))
+  list_1=$( echo ${list_names[@]} \
+    | xargs -t -n 1 dig +short a \
+    | grep -v '[a-z]' 
+  )
+  list_2=$( echo $list_names | xargs -n1 echo |  grep  -E '[0-9]{1,3}$')
+  for i in $( echo $list_1 $list_2 | sort -n | uniq ); do echo $i; done  | tee dns-list-dot-as-ip.txt
+}
+function dos_to_ip(){
+  dot_to_ip
+  doh_to_ip
+}
+function ignore_list(){
+ ARR=();
+ for i  in $(cat  ignore-list.txt | sort | uniq  ) ; do
+  ARR+=( $i );
+ done ;
+ list=$( IFS=\|; echo "${ARR[*]}" );
+ echo "${list}"
+}
+
 function main() {
 
   list=$(get_known_dns_list | sort | uniq)
@@ -17,6 +60,8 @@ function main() {
    | sed 's|/dns-query||' \
    | sed -r 's%(https|tls|quic)://%%' \
    | sort | uniq | tee dns-list-domain.txt
+  # 
+  dos_to_ip
 
 }
 
